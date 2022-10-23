1d2807dc20a7072b27298c7549771e8ad5069d6d
ad-tra <you@example.com>
2022-10-23 18:34:26 -0400

    #1 initialized custom theme

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,52 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use eframe::egui;
+use eframe::egui::{FontId, TextStyle, FontFamily};
+use eframe::emath::Align;
+use eframe::epaint::Color32;
 
 fn main() {
-    let options = eframe::NativeOptions::default();
+    let mut options = eframe::NativeOptions::default();
     eframe::run_native(
         "Take Home",
         options,
-        Box::new(|_cc| Box::new(MyApp::default())),
+        Box::new(|cc| Box::new(MyApp::new(cc))),
     );
+}
+
+//fonts, and colors
+fn configure_custom_theme(ctx: &egui::Context) {
+    
+    let mut fonts = egui::FontDefinitions::default();
+    let mut style = (*ctx.style()).clone();
+
+
+    fonts.font_data.insert("sabo_filled".to_owned(),egui::FontData::from_static(include_bytes!("../assets/fonts/Sabo-Filled.otf")));
+    fonts.font_data.insert("sabo_regular".to_owned(), egui::FontData::from_static(include_bytes!("../assets/fonts/Sabo-Regular.otf")));
+    fonts.font_data.insert("cascadia_regular".to_owned(), egui::FontData::from_static(include_bytes!("../assets/fonts/Cascadia-Regular.otf")));
+    
+    fonts.families.entry(FontFamily::Name("Sabo".into())).or_default().push("sabo_filled".to_owned());
+    fonts.families.entry(egui::FontFamily::Monospace).or_default().insert(0, "cascadia_regular".to_owned());
+
+
+
+    style.text_styles = [
+        (TextStyle::Heading, FontId::new(60.0, FontFamily::Name(("Sabo").into()))),
+        (TextStyle::Body, FontId::new(18.0, FontFamily::Monospace)),
+        (TextStyle::Monospace, FontId::new(18.0, FontFamily::Monospace)),
+        (TextStyle::Button, FontId::new(18.0, FontFamily::Monospace)),
+        (TextStyle::Small, FontId::new(12.0, FontFamily::Monospace)),
+    ].into();
+    
+
+    let mut visuals = egui::Visuals::default();
+    visuals.override_text_color = Some(Color32::from_rgb(248, 248, 242));
+    visuals.widgets.noninteractive.bg_fill = Color32::from_rgb(40, 42, 54);
+
+    ctx.set_style(style);
+    ctx.set_visuals(visuals);
+    ctx.set_fonts(fonts);
+
 }
 
 struct MyApp {
@@ -16,8 +54,9 @@ struct MyApp {
     age: u32,
 }
 
-impl Default for MyApp {
-    fn default() -> Self {
+impl  MyApp {
+    fn new(cc : &eframe::CreationContext<'_>) -> Self {
+        configure_custom_theme(&cc.egui_ctx);
         Self {
             name: "Arthur".to_owned(),
             age: 42,
@@ -27,8 +66,12 @@ impl Default for MyApp {
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+
+
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Hello world");
+            ui.with_layout(egui::Layout::right_to_left(Align::LEFT), |ui|{
+                ui.heading("Your Habit tracker");
+            });
             ui.horizontal(|ui| {
                 ui.label("Your name: ");
                 ui.text_edit_singleline(&mut self.name);
