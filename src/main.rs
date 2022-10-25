bbaaeb32c0e26e7b78487c1cfc9e9d7b708a331c
ad-tra <you@example.com>
2022-10-24 21:49:43 -0400

    #1 added a static habit frame

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use eframe::egui;
+
+use eframe::egui::{self, Layout, RichText, Frame};
+use eframe::egui::style::Margin;
 use eframe::egui::{FontId, TextStyle, FontFamily};
 use eframe::emath::Align;
-use eframe::epaint::Color32;
+use eframe::epaint::{Color32, Rounding, Shadow, Stroke};
 
 fn main() {
     let mut options = eframe::NativeOptions::default();
+    options.decorated = true; 
+    options.fullscreen = true;
+    
     eframe::run_native(
         "Take Home",
         options,
@@ -31,10 +36,10 @@ fn configure_custom_theme(ctx: &egui::Context) {
 
 
     style.text_styles = [
-        (TextStyle::Heading, FontId::new(60.0, FontFamily::Name(("Sabo").into()))),
+        (TextStyle::Heading, FontId::new(50.0, FontFamily::Name(("Sabo").into()))),
         (TextStyle::Body, FontId::new(18.0, FontFamily::Monospace)),
         (TextStyle::Monospace, FontId::new(18.0, FontFamily::Monospace)),
-        (TextStyle::Button, FontId::new(18.0, FontFamily::Monospace)),
+        (TextStyle::Button, FontId::new(25.0,  FontFamily::Name(("Sabo").into()))),
         (TextStyle::Small, FontId::new(12.0, FontFamily::Monospace)),
     ].into();
     
@@ -42,6 +47,8 @@ fn configure_custom_theme(ctx: &egui::Context) {
     let mut visuals = egui::Visuals::default();
     visuals.override_text_color = Some(Color32::from_rgb(248, 248, 242));
     visuals.widgets.noninteractive.bg_fill = Color32::from_rgb(40, 42, 54);
+    visuals.button_frame = false;
+    
 
     ctx.set_style(style);
     ctx.set_visuals(visuals);
@@ -67,20 +74,59 @@ impl  MyApp {
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
 
-
-        egui::CentralPanel::default().show(ctx, |ui| {
+        let root_frame = egui::Frame {
+            inner_margin: Margin::symmetric(50.00, 50.00),
+            outer_margin: Margin::same(0.0),
+            rounding: Rounding::same(0.0),
+            shadow: Shadow::small_dark(),
+            fill: Color32::from_rgb(40, 42, 54),
+            stroke: Stroke::none() 
+        };
+        egui::CentralPanel::default().frame(root_frame).show(ctx, |ui| {
+            
             ui.with_layout(egui::Layout::right_to_left(Align::LEFT), |ui|{
                 ui.heading("Your Habit tracker");
             });
-            ui.horizontal(|ui| {
-                ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name);
-            });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Click each year").clicked() {
-                self.age += 1;
-            }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+            
+            //TODO: refactor this "habit" frame into a widget/component to make code dry  
+            Frame::default()
+                .inner_margin(Margin::same(25.00))
+                .fill(Color32::from_rgb(68, 71, 90))
+                .outer_margin(Margin::symmetric(0.0, 30.0))
+                .show(ui,|ui| { ui.with_layout(Layout::default().with_cross_justify(true), |ui|{
+
+                    ui.heading("Drawing");
+                    ui.label("be one with my pencil. learn about anatomy, perspective, and color theory");
+
+                    
+                    Frame::default()
+                    .outer_margin(Margin{left:0.0, right:0.0, bottom:0.0, top: 150.0})
+                    .show(ui,|ui| { ui.with_layout(Layout::left_to_right(Align::Min), |ui|{
+                        ui.add(egui::Button::new(RichText::new("Add Entry +").color(Color32::from_rgb(139, 233, 253)).underline()));
+                        ui.add_space(15.0);
+                        ui.add(egui::Button::new(RichText::new("Tick the day").underline()));
+                    })});                    
+            })});
+
+            Frame::default()
+                .inner_margin(Margin::same(25.00))
+                .fill(Color32::from_rgb(68, 71, 90))
+                .outer_margin(Margin::symmetric(0.0, 30.0))
+                .show(ui,|ui| { ui.with_layout(Layout::default().with_cross_justify(true), |ui|{
+
+                    ui.heading("Read");
+                    ui.label("crack the books, learn something new.");
+
+                    
+                    Frame::default()
+                    .outer_margin(Margin{left:0.0, right:0.0, bottom:0.0, top: 150.0})
+                    .show(ui,|ui| { ui.with_layout(Layout::left_to_right(Align::Min), |ui|{
+                        ui.add(egui::Button::new(RichText::new("Add Entry +").color(Color32::from_rgb(241, 250, 140)).underline()));
+                        ui.add_space(15.0);
+                        ui.add(egui::Button::new(RichText::new("Tick the day").underline()));
+                    })});                    
+            })});
+
         });
     }
 }
