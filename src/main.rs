efdf70a32c39b88a4548f05e88651c4b664b4f7e
ad-tra <aew31gcmuy@gmail.com>
2022-11-02 22:46:38 -0400

    #3 WIP made frequency cron and started building the grid based on freq and creation date

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use cron::Schedule;
 use uuid::Uuid;
 use std::fs;
+use std::str::FromStr;
 use chrono::prelude::*;
 use serde::{Deserialize, Serialize};
 
@@ -23,133 +25,32 @@ fn main() {
 }
 
 
-//fonts, and colors
-fn configure_custom_theme(ctx: &egui::Context) {
-    
-    let mut fonts = egui::FontDefinitions::default();
-    let mut style = (*ctx.style()).clone();
-
-
-    fonts.font_data.insert("sabo_filled".to_owned(),egui::FontData::from_static(include_bytes!("../assets/fonts/Sabo-Filled.otf")));
-    fonts.font_data.insert("sabo_regular".to_owned(), egui::FontData::from_static(include_bytes!("../assets/fonts/Sabo-Regular.otf")));
-    fonts.font_data.insert("cascadia_regular".to_owned(), egui::FontData::from_static(include_bytes!("../assets/fonts/Cascadia-Regular.otf")));
-    
-    fonts.families.entry(FontFamily::Name("Sabo".into())).or_default().push("sabo_filled".to_owned());
-    fonts.families.entry(FontFamily::Name("SaboRegular".into())).or_default().push("sabo_regular".to_owned());
-    fonts.families.entry(egui::FontFamily::Monospace).or_default().insert(0, "cascadia_regular".to_owned());
-
-
-
-    style.text_styles = [
-        (TextStyle::Heading, FontId::new(50.0, FontFamily::Name(("Sabo").into()))),
-        (TextStyle::Name("Heading2Filled".into()), FontId::new(30.0, FontFamily::Name(("Sabo").into()))),
-        (TextStyle::Name("Heading2Regular".into()), FontId::new(30.0, FontFamily::Name(("SaboRegular").into()))),
-        (TextStyle::Body, FontId::new(18.0, FontFamily::Monospace)),
-        (TextStyle::Monospace, FontId::new(18.0, FontFamily::Monospace)),
-        (TextStyle::Button, FontId::new(25.0,  FontFamily::Name(("Sabo").into()))),
-        (TextStyle::Small, FontId::new(12.0, FontFamily::Monospace)),
-    ].into();
-    
-
-    let mut visuals = egui::Visuals::default();
-    visuals.override_text_color = Some(Color32::from_rgb(248, 248, 242));
-    visuals.widgets.noninteractive.bg_fill = Color32::from_rgb(40, 42, 54);
-    visuals.button_frame = false;
-    
-
-    ctx.set_style(style);
-    ctx.set_visuals(visuals);
-    ctx.set_fonts(fonts);
-
-}
-
-
-#[derive(Serialize, Deserialize, Debug,  Clone)]
-struct Habit{
-    id: Uuid,
-    name: String,
-    description: String,
-    color: Color32,
-    streak: u32,
-    frequency: String,
-    created_at: Option<DateTime<Utc>>,
-}
-impl Habit{
-    fn default() -> Self {
-        Habit{
-            id: Uuid::new_v4(),
-            color: Color32::from_rgb(139, 233, 253),
-            name: "".to_owned(),
-            description: "".to_owned(),
-            streak: 0,
-            frequency: "".to_owned(),
-            created_at: None,
-        }
-    }
-    fn show(&self, ui : &mut egui::Ui){
-        
-        Frame::default()
-            .inner_margin(Margin::symmetric(40.00, 25.00))
-            .fill(Color32::from_rgb(68, 71, 90))
-            .outer_margin(Margin::symmetric(0.0, 30.0))
-            .show(ui,|ui| { ui.with_layout(Layout::default().with_cross_justify(true), |ui|{
-
-
-                //this whole dance is to achieve the effect of justify-content:space-between
-                //TODO: abstract this into a macro?? it would be nice to shorten this syntax because spacing two elements between each other on a line would be used in high frequency in the future.   
-                ui.with_layout(Layout::left_to_right(Align::TOP).with_main_justify(true), |ui|{
-                
-                    ui.with_layout(Layout::left_to_right(Align::TOP), |ui|{
-                        ui.heading(&self.name);
-                    });
-                    
-                    ui.with_layout(Layout::right_to_left(Align::TOP), |ui|{
-                        
-                        ui.label(RichText::new("Day Streak").text_style(TextStyle::Name("Heading2Filled".into())));
-                        ui.label(RichText::new(self.streak.to_string()).text_style(TextStyle::Name("Heading2Regular".into())));
-                        ui.label(RichText::new("•••").color(self.color).text_style(TextStyle::Name("Heading2Filled".into())));
-
-                    });
-                });
-                ui.label(&self.description);
-
-
-                ui.allocate_space(vec2(0.0, 20.0));
-                CalendarGrid::new(24, 8, self.color).show(ui);
-
-                //TODO refactor using ui.allocate_space instead of frame margin and ui.horizontal instead of ui.with_layout. it will make this block more concise 
-                Frame::default()
-                .outer_margin(Margin{left:0.0, right:0.0, bottom:0.0, top: 20.0})
-                .show(ui,|ui| { ui.with_layout(Layout::left_to_right(Align::Min), |ui|{
-                    ui.add(egui::Button::new(RichText::new("Add Entry +").color(self.color).underline()));
-                    ui.add_space(15.0);
-                    ui.add(egui::Button::new(RichText::new("Tick the day").underline()));
-                })});   
-
-                
-        })});
-    }
-}
-
 struct MyApp {
     new_habit:  Habit,
     habits: Vec<Habit>,
+    actions: Vec<Action>,
     is_new_habit_window_visible: bool,
 }
 
 impl  MyApp {
     fn new(cc : &eframe::CreationContext<'_>) -> Self {
         configure_custom_theme(&cc.egui_ctx);
+        let res : serde_json::Value = serde_json::from_reader(fs::File::open("habits.json").unwrap()).unwrap();
         Self {
             is_new_habit_window_visible: false,
             new_habit: Habit::default(),
-            habits: serde_json::from_str(std::str::from_utf8(&fs::read("habits.json").unwrap()).unwrap()).unwrap(),
+            habits: Vec::<Habit>::deserialize(&res["habits"]).unwrap(),
+            actions:  Vec::<Action>::deserialize(&res["actions"]).unwrap()
+            
+            
         }
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        
+
 
         let root_frame = egui::Frame {
             inner_margin: Margin::symmetric(50.00, 50.00),
@@ -220,9 +121,8 @@ impl eframe::App for MyApp {
 
                                         self.new_habit.created_at = Some(Utc::now());
                                         self.habits.push(self.new_habit.clone());
+                                        
                                         fs::write("habits.json", serde_json::to_string(&self.habits).unwrap()).expect("should be able to write content to habits.json");
-                                        //let p: Vec<Habit> = serde_json::from_str(std::str::from_utf8(&fs::read("habits.json").unwrap()).unwrap()).unwrap();
-                                        //println!("{:#?}", p);
 
                                         self.new_habit = Habit::default();
                                         self.is_new_habit_window_visible =false;
@@ -257,11 +157,11 @@ impl eframe::App for MyApp {
             
             
             ui.with_layout(egui::Layout::right_to_left(Align::LEFT), |ui|{
-                ui.heading("good night maha!");
+                ui.heading("your habit tracker");
             });
 
             ScrollArea::new([false, true]).show(ui, |ui|{
-                for habit in self.habits.iter()  {habit.show(ui)};
+                for habit in self.habits.iter()  {habit.show(ui, &mut self.actions)};
             
             });
             
@@ -283,6 +183,81 @@ impl eframe::App for MyApp {
     }
     
 }
+#[derive(Serialize, Deserialize, Debug,  Clone)]
+struct Habit{
+    id: Uuid,
+    name: String,
+    description: String,
+    color: Color32,
+    streak: u32,
+    frequency: String,
+    created_at: Option<DateTime<Utc>>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct Action{
+    habit_id: Uuid,
+    created_at: DateTime<Utc>
+}
+
+impl Habit{
+    fn default() -> Self {
+        Habit{
+            id: Uuid::new_v4(),
+            color: Color32::from_rgb(139, 233, 253),
+            name: "".to_owned(),
+            description: "".to_owned(),
+            streak: 0,
+            frequency: "".to_owned(),
+            created_at: None,
+        }
+    }
+    fn show(&self, ui : &mut egui::Ui, actions: &mut Vec<Action>){
+        
+        Frame::default()
+            .inner_margin(Margin::symmetric(40.00, 25.00))
+            .fill(Color32::from_rgb(68, 71, 90))
+            .outer_margin(Margin::symmetric(0.0, 30.0))
+            .show(ui,|ui| { ui.with_layout(Layout::default().with_cross_justify(true), |ui|{
+
+
+                //this whole dance is to achieve the effect of justify-content:space-between
+                //TODO: abstract this into a macro?? it would be nice to shorten this syntax because spacing two elements between each other on a line would be used in high frequency in the future.   
+                ui.with_layout(Layout::left_to_right(Align::TOP).with_main_justify(true), |ui|{
+                
+                    ui.with_layout(Layout::left_to_right(Align::TOP), |ui|{
+                        ui.heading(&self.name);
+                    });
+                    
+                    ui.with_layout(Layout::right_to_left(Align::TOP), |ui|{
+                        
+                        ui.label(RichText::new("Day Streak").text_style(TextStyle::Name("Heading2Filled".into())));
+                        ui.label(RichText::new(self.streak.to_string()).text_style(TextStyle::Name("Heading2Regular".into())));
+                        ui.label(RichText::new("•••").color(self.color).text_style(TextStyle::Name("Heading2Filled".into())));
+
+                    });
+                });
+                ui.label(&self.description);
+
+
+                ui.allocate_space(vec2(0.0, 20.0));
+                CalendarGrid::new(24, 8, self.color).show(ui,self.created_at.unwrap(), Schedule::from_str(self.frequency.as_str()).unwrap(), actions);
+
+                //TODO refactor using ui.allocate_space instead of frame margin and ui.horizontal instead of ui.with_layout. it will make this block more concise 
+                Frame::default()
+                .outer_margin(Margin{left:0.0, right:0.0, bottom:0.0, top: 20.0})
+                .show(ui,|ui| { ui.with_layout(Layout::left_to_right(Align::Min), |ui|{
+                    ui.add(egui::Button::new(RichText::new("Add Entry +").color(self.color).underline()));
+                    ui.add_space(15.0);
+                    ui.add(egui::Button::new(RichText::new("Tick the day").underline()));
+                })});   
+
+                
+        })});
+    }
+}
+
+
 
 
 //TODO: add validation, and make a form struct that spawns controlled inputs
@@ -325,7 +300,6 @@ impl<'a> ControlledInput<'a>{
 
 
 
-
 struct CalendarGrid{
     cols: u32,
     rows: u32,
@@ -338,7 +312,36 @@ impl CalendarGrid{
             cols,rows,done_color
         }
     }
-    fn show(&self,  ui : &mut egui::Ui){
+    fn show(&self, ui: &mut egui::Ui, created_at: DateTime<Utc>, schedule: Schedule, actions : &mut Vec<Action>){
+        let mut fire_times = schedule.after(&created_at).take_while(|&x| Utc::now().signed_duration_since(x).num_seconds().is_positive());
+        let available_slots = self.rows * self.cols;
+
+
+        // if fire_times.count() > (self.rows * self.cols ) as usize {
+        //     //the habit has been running for a long time. it exceeds the available slots, TODO add a scrolling mechanism between slot-windows
+        // };
+
+
+
+        // let mut vec : Vec<bool> = vec![false; available_slots as usize];
+        
+        // for (i, cell) in vec.iter_mut().enumerate(){
+        //     //TODO handle when i+1 is out of bounds
+        //     let start_range = fire_times.nth(i).unwrap();
+        //     let end_range = fire_times.nth(i+1).unwrap();
+
+        //     //determine if some date_time is contained inside of a range
+        //     //date_time represents an action 
+        //     let action = &actions[0];
+        //     let is_inside_range = action.created_at.signed_duration_since(start_range).num_seconds().is_negative() && action.created_at.signed_duration_since(end_range).num_seconds().is_positive();
+        
+
+        //     if is_inside_range {
+        //         actions.remove(0);
+        //         *cell = true
+        //     } 
+        // }
+
         for i in 0..self.rows{
             ui.spacing_mut().item_spacing = vec2(6.0, 6.0);
             ui.with_layout(Layout::right_to_left(Align::Min), |ui|{
@@ -351,4 +354,44 @@ impl CalendarGrid{
             });
         }
     }
+}
+
+//fonts, and colors
+fn configure_custom_theme(ctx: &egui::Context) {
+    
+    let mut fonts = egui::FontDefinitions::default();
+    let mut style = (*ctx.style()).clone();
+
+
+    fonts.font_data.insert("sabo_filled".to_owned(),egui::FontData::from_static(include_bytes!("../assets/fonts/Sabo-Filled.otf")));
+    fonts.font_data.insert("sabo_regular".to_owned(), egui::FontData::from_static(include_bytes!("../assets/fonts/Sabo-Regular.otf")));
+    fonts.font_data.insert("cascadia_regular".to_owned(), egui::FontData::from_static(include_bytes!("../assets/fonts/Cascadia-Regular.otf")));
+    
+    fonts.families.entry(FontFamily::Name("Sabo".into())).or_default().push("sabo_filled".to_owned());
+    fonts.families.entry(FontFamily::Name("SaboRegular".into())).or_default().push("sabo_regular".to_owned());
+    fonts.families.entry(egui::FontFamily::Monospace).or_default().insert(0, "cascadia_regular".to_owned());
+
+
+
+    style.text_styles = [
+        (TextStyle::Heading, FontId::new(50.0, FontFamily::Name(("Sabo").into()))),
+        (TextStyle::Name("Heading2Filled".into()), FontId::new(30.0, FontFamily::Name(("Sabo").into()))),
+        (TextStyle::Name("Heading2Regular".into()), FontId::new(30.0, FontFamily::Name(("SaboRegular").into()))),
+        (TextStyle::Body, FontId::new(18.0, FontFamily::Monospace)),
+        (TextStyle::Monospace, FontId::new(18.0, FontFamily::Monospace)),
+        (TextStyle::Button, FontId::new(25.0,  FontFamily::Name(("Sabo").into()))),
+        (TextStyle::Small, FontId::new(12.0, FontFamily::Monospace)),
+    ].into();
+    
+
+    let mut visuals = egui::Visuals::default();
+    visuals.override_text_color = Some(Color32::from_rgb(248, 248, 242));
+    visuals.widgets.noninteractive.bg_fill = Color32::from_rgb(40, 42, 54);
+    visuals.button_frame = false;
+    
+
+    ctx.set_style(style);
+    ctx.set_visuals(visuals);
+    ctx.set_fonts(fonts);
+
 }
