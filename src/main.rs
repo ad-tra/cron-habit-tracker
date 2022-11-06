698f11ebe80e9efc8e882fdba88bba562fedc896
ad-tra <aew31gcmuy@gmail.com>
2022-11-06 00:16:53 -0400

    #5 added colors to differentiate between habit states

--- src/main.rs
+++ src/main.rs
@@ -257,7 +257,7 @@ impl Habit{
                         fs::write("actions.json", serde_json::to_string(&actions).unwrap()).expect("should be able to write content to habits.json");
                     }
                     if mark_done.hovered() && is_action_done {
-                        egui::show_tooltip_text(ui.ctx(), egui::Id::new("my_tooltip"), " you already did this. Good Job!");
+                        egui::show_tooltip_text(ui.ctx(), egui::Id::new("my_tooltip"), " you  did this. Good Job!");
 
                     }
                     ui.add_space(15.0);
@@ -322,6 +322,12 @@ struct CalendarGrid{
     done_color: Color32,
 }
 
+#[derive(Clone,PartialEq)]
+enum Cell {
+    Done,
+    NotDone,
+    OutOfBounds,
+}
 impl CalendarGrid{
     fn new(cols: u32, rows: u32, done_color: Color32) -> Self {
         Self {
@@ -334,12 +340,15 @@ impl CalendarGrid{
         let fire_times  = schedule.after(&habit.created_at.unwrap()).take_while(|&x| Utc::now().signed_duration_since(x).num_seconds().is_positive()).count()+1;
         let mut fire_times: Vec<DateTime<Utc>> = schedule.after(&habit.created_at.unwrap()).take(fire_times).collect(); 
         fire_times.reverse();
+        fire_times.push(habit.created_at.unwrap());
+        
+
 
         let mut actions : Vec<&Action> =  actions.iter().filter(|x| x.habit_id == habit.id).collect();
         actions.sort_by(|a,b| b.created_at.cmp(&a.created_at));
 
         let available_slots = self.rows * self.cols;
-        let mut flat_grid : Vec<bool> = vec![false; available_slots  as usize];
+        let mut flat_grid : Vec<Cell> = vec![Cell::OutOfBounds; available_slots  as usize];
         
 
         if fire_times.len() > available_slots as usize {
@@ -349,6 +358,7 @@ impl CalendarGrid{
         let mut  is_action_done = false;
 
         for (i, cell) in flat_grid.iter_mut().enumerate(){
+            println!("{:#?}", actions);
 
             let start_range = match fire_times.get(i){
                 Some(e) => *e,
@@ -359,7 +369,6 @@ impl CalendarGrid{
             };
             let end_range = match fire_times.get(i+1){
                 Some(e) => *e,
-                None if fire_times.len() == 1 => habit.created_at.unwrap(),
                 None => {
                     println!("no end range");
                     break;
@@ -369,26 +378,28 @@ impl CalendarGrid{
                 Some(e) => *e,
                 None => {
                     println!("no action");
-                    break;
+                    *cell = Cell::NotDone;
+                    continue;
                 },
             };
 
 
-
-            println!("\n\nstart: {}\naction: {}\nend: {}", start_range, action.created_at, end_range);
+            println!("start: {:#?}\nend: {:#?}", start_range, end_range);
             
             //check if action date time is contained in the range of two consecutive fire times
             let is_inside_range = action.created_at.signed_duration_since(start_range).num_seconds().is_negative() && action.created_at.signed_duration_since(end_range).num_seconds().is_positive();
-            println!("is inside: {}" , is_inside_range);
 
             if i == 0 {
                 is_action_done = is_inside_range;
             }
-
+            println!("{}", is_inside_range);
             if is_inside_range {
-                *cell = true;
+                *cell = Cell::Done;
                 actions.remove(0);
-            } 
+            }
+            else{
+                *cell = Cell::NotDone;
+            }
         }
 
 
@@ -399,7 +410,14 @@ impl CalendarGrid{
                 for j in 0..self.cols{
                     
                     let rect = ui.allocate_exact_size(vec2(18.0, 18.0), Sense::hover()).0;
-                    ui.painter().rect(rect, Rounding::default(), if flat_grid[(i *self.cols ) as usize + j as usize] == true {self.done_color} else {Color32::from_rgb(104, 107, 120)}, Stroke::none());
+                    
+                    let cell_color =  match flat_grid[(i *self.cols ) as usize + j as usize]{
+                        Cell::Done => self.done_color,
+                        Cell::NotDone => Color32::from_rgb(104, 107, 120),
+                        Cell::OutOfBounds => Color32::from_rgb(104, 107, 120).linear_multiply(0.2),
+                    };
+
+                    ui.painter().rect(rect, Rounding::default(), cell_color, Stroke::none());
 
                 }
             });
